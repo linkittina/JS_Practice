5105285ddeee74e6f763e80aff67c16e497d913e
linkittina <lin.kittina@gmail.com>
2020-11-30 22:21:48 +0100

    Reorder comment section and pretty format task_01_number_of_bits.js file.

--- Practice_1/tasks/task_01_number_of_bits.js
+++ Practice_1/tasks/task_01_number_of_bits.js
@@ -1,34 +1,37 @@
 'use strict';
 
-/**
- * The function determines and returns the
- * number of 1 digits in the binary representation
- * of the given number.
- *
- * @param {number} value (value > 0)
- * @returns {number} the number of 1 bits or 0 if any arguments are not proper
- */
+/*
+* The function determines and returns the
+* number of 1 digits in the binary representation
+* of the given number.
+*
+* @param {number} value (value > 0)
+* @returns {number} the number of 1 bits or 0 if any arguments are not proper
+*/
+
+/*
+* Your task is to determine how many
+* 1 bits are there in the binary representation
+* of the number stored in the variable,
+* called value. If you determined it,
+* assign it to the variable, called bitNumber.
+* 
+* E.g.
+*   value = 93 -> 0b1011101
+*   bitNumber = 5
+*/
+
 function numberOfBits(value) {
     let bitNumber = 0;
-    /*
-     * Your task is to determine how many
-     * 1 bits are there in the binary representation
-     * of the number stored in the variable,
-     * called value. If you determined it,
-     * assign it to the variable, called bitNumber.
-     * 
-     * E.g.
-     *   value = 93 -> 0b1011101
-     *   bitNumber = 5
-     */
-    // PLACE YOUR CODE BETWEEN THIS...
     let binary = value.toString(2);
+
     for (var i = 0; i < binary.length; i++) {
         if (binary[i] === '1' ) {
             bitNumber ++;
         }
     }
-    // ...AND THIS COMMENT LINE!
+
     return bitNumber;
 }
+
 module.exports = numberOfBits;
