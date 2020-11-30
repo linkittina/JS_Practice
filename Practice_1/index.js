fb24bd35e1183112dd98bba7450b415ef8dca2d0
Kitti Linter <40621706+linkittina@users.noreply.github.com>
2020-12-01 00:14:40 +0100

    Solution for the following task: task_02_euclidean_algorithm.

--- Practice_1/index.js
+++ Practice_1/index.js
@@ -4,3 +4,7 @@ console.log('Executing your tests:')
 const numberOfBits = require('./tasks/task_01_number_of_bits');
 console.log('It\'s a:', numberOfBits);
 console.log(numberOfBits(1556));
+
+const euclidean = require('./tasks/task_02_euclidean_algorithm');
+console.log('It\'s a:', euclidean);
+console.log(euclidean(64, 8));
